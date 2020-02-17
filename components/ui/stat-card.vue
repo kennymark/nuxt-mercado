ae6b28e06ec6a9a729dbfe3f10d560b038509e5d
Kenny Mark <geniounico@outlook.com>
2020-02-17 13:52:37 +0000

    tweaked admin stat cards

--- components/ui/stat-card.vue
+++ components/ui/stat-card.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="px-5 py-1 shadow-sm rounded-lg bg-white w-full h-auto">
-    <p class="text-xl text-gray-700">{{data.title}}</p>
+    <p class="text-xl text-gray-800">{{data.title}}</p>
     <p class="text-sm text-gray-600">{{data.description}}</p>
-    <h3 class="text-black text-4xl font-bold text-gray-800">{{data.text | currency('£')}}</h3>
+    <h3 class="text-black text-4xl font-medium text-gray-700">{{data.text | currency('£')}}</h3>
     <slot />
   </div>
 </template>
