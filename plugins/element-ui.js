bd5c3b45a54b03dd4c3d9c259306565e41fa87e2
Kenny Mark <geniounico@outlook.com>
2020-02-20 18:34:21 +0000

    test again

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
-
-Vue.use(Element, { locale })
+if (process.BROWSER_BUILD) {
+  Vue.use(Element, { locale })
+}
